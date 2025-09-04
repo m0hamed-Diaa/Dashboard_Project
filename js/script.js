06905ab3c7521590dcd8aaae058c8cb57ec2f952
m0hamed-Diaa <mohameddiaaahdahed@gmail.com>
2025-09-04 18:52:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// Start sidebar MenuLinks
+let sidebarMenuLinks = document.querySelectorAll("#sidebarMenuLinks li")
+
+if (localStorage.getItem("sidebarMenuLinks")) {
+    sidebarMenuLinks.forEach((li) => {
+        li.classList.remove("active");
+    })
+    document.querySelector(`[data-asidebar='${localStorage.getItem("sidebarMenuLinks")}']`).classList.add("active");
+}
+sidebarMenuLinks.forEach((li) => {
+    li.addEventListener("click", (e) => {
+        sidebarMenuLinks.forEach((li) => {
+            li.classList.remove("active");
+        })
+        e.currentTarget.classList.add("active");
+        localStorage.setItem("sidebarMenuLinks", e.currentTarget.dataset.asidebar);
+    })
+})
+
+// End sidebar MenuLinks
+// Start nav menu icon
+let navMenuIcon = document.querySelector(".bx-menu");
+navMenuIcon.onclick = function () {
+    document.querySelector("#sidebar").classList.toggle("active");
+}
+// End nav menu icon
+// Start Dark Mode
+let inputCheckbox = document.querySelector("[type='checkbox']");
+if (localStorage.getItem("dark") === "done") {
+    document.documentElement.classList.add("dark");
+    inputCheckbox.setAttribute("checked", true);
+}
+inputCheckbox.onclick = function() {
+    document.documentElement.classList.toggle("dark");
+    const isConatins = document.documentElement.classList.contains("dark");
+    localStorage.setItem("dark", isConatins ? "done": "notDone");
+}
+// End Dark Mode
+// Start notificationIcon 
+let notificationIcon = document.querySelector("#notificationIcon");
+let profileIcon = document.querySelector("#profileIcon");
+notificationIcon.onclick = function () {
+    document.querySelector("#notificationMenu").classList.toggle("active");
+}
+profileIcon.onclick = function () {
+    document.querySelector("#profileMenu").classList.toggle("active");
+}
+// End notificationIcon 
+// Start display-none-search 
+let display_none_search = document.querySelector(".display-none-search");
+display_none_search.onclick = function () {
+    document.querySelector("#navSearch").classList.toggle("active"); 
+    display_none_search.classList.toggle("active");
+}
+// End display-none-search 
